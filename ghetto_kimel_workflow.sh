90add72c704068e76881f1b2f35bae858ac6ca91
Erin Dickie <Erin.Dickie@camh.ca>
2022-10-14 14:59:18 -0400

    making participant workflow a submission script

--- ghetto_kimel_workflow.sh
+++ ghetto_kimel_workflow.sh
@@ -1,10 +1,24 @@
+#!/bin/bash -l
+
+#SBATCH --partition=low-moby
+#SBATCH --array=1-188
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4096
+#SBATCH --time=2:00:00
+#SBATCH --job-name enigmaDTI
+#SBATCH --output=enigmaDTI_%j.out
+#SBATCH --error=enigmaDTI_%j.err
+
 # kimel lab all the things
 
 module load R FSL ENIGMA-DTI/2015.01
+module load ciftify
 
-subject_id="CMH00000086"
 session="ses-01"
 STUDY="TAY"
+
+OUTPUT_RESOLUTION="1.7"
 
 SING_CONTAINER=/archive/code/containers/QSIPREP/pennbbl_qsiprep_0.16.0RC3-2022-06-03-9c3b9f2e4ac1.simg
 
@@ -16,6 +30,10 @@ FS_LICENSE=${TMP_DIR}/freesurfer_license/license.txt
 OUT_DIR=/scratch/edickie/TAY_engimaDTI/data
 
 mkdir -p $WORK_DIR $OUT_DIR
+
+THIS_DWI=`ls -1d ${QSIPREP_DIR}/sub-*/ses-01/dwi/*desc-preproc_dwi.nii.gz | head -n ${SLURM_ARRAY_TASK_ID} | tail -n 1`
+subject=$(basename $(dirname $(dirname $(dirname ${THIS_DWI}))))
+subject_id=$(echo $subject | sed 's/sub-//g')
 
 singularity run \
   -H ${TMP_DIR} \
@@ -32,7 +50,7 @@ singularity run \
   --recon-only \
   --recon-spec reorient_fslstd \
   --recon-input /qsiprep_in \
-  --output-resolution 1.7 \
+  --output-resolution ${OUTPUT_RESOLUTION} \
   --fs-license-file /li \
   -w /work \
   --notrack
@@ -46,9 +64,10 @@ dtifit -k ${QSIRECON_OUT}_dwi.nii.gz \
   -m ${QSIRECON_OUT}_mask.nii.gz \
   -r ${QSIRECON_OUT}_dwi.bvec \
   -b ${QSIRECON_OUT}_dwi.bval \
-  --save_tensor -o ${DTIFIT_OUT}
+  --save_tensor --sse \
+  -o ${DTIFIT_OUT}
 
-module load ciftify
+
 
 ENIGMA_DTI_OUT=/scratch/edickie/TAY_engimaDTI/data/engimaDTI
 
@@ -57,7 +76,7 @@ mkdir -p ${ENIGMA_DTI_OUT}
 python /scratch/edickie/TAY_engimaDTI/ENIGMA_DTI_BIDS/run_participant_enigma_extract.py --calc-all --debug \
   ${ENIGMA_DTI_OUT}/sub-${subject_id}_${session} ${DTIFIT_OUT}_FA.nii.gz
 
-####### group steps
+# ####### group steps
 
-python /scratch/edickie/TAY_engimaDTI/ENIGMA_DTI_BIDS/run_group_enigma_concat.py \
- ${ENIGMA_DTI_OUT} FA ${ENIGMA_DTI_OUT}/group_engimaDTI_FA.csv
+# python /scratch/edickie/TAY_engimaDTI/ENIGMA_DTI_BIDS/run_group_enigma_concat.py \
+#  ${ENIGMA_DTI_OUT} FA ${ENIGMA_DTI_OUT}/group_engimaDTI_FA.csv
